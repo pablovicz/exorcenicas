b40001ecaef7555acbb01e2b38a6c4ae099d9ab3
Pablo Woinarovicz Ramos <pablowoina2205@gmail.com>
2022-09-22 16:32:31 -0300

    Corrections

--- exorcenicas-webapp/src/components/Footer.tsx
+++ exorcenicas-webapp/src/components/Footer.tsx
@@ -1,9 +1,18 @@
-import { Flex, Icon, Link, Text, VStack } from "@chakra-ui/react";
+import { Flex, Icon, Link, Text, useBreakpointValue, VStack } from "@chakra-ui/react";
 import { GrInstagram } from "react-icons/gr";
 
 
 
 export function Footer() {
+
+    const isMobileVersion = useBreakpointValue({
+        base: false,
+        sm: true,
+        md: false,
+        lg: false,
+        xl: false
+    });
+
 
     return (
         <VStack spacing='2' w='full' as='footer' mt='20'>
@@ -13,9 +22,9 @@ export function Footer() {
                 target="_blank"
                 rel='noopener noreferer'
             >
-                <Flex flexDir='row' align='center' justify='space-between' w='100%'>
-                    <Icon as={GrInstagram} fontSize='1rem' />
-                    <Text ml='4' fontSize='1.5rem' fontWeight='regular' textAlign='left' lineHeight='120%'>
+                <Flex flexDir={isMobileVersion ? 'column' : 'row'} align='center' justify='space-between' w='100%'>
+                    <Icon as={GrInstagram} fontSize={isMobileVersion ? '2rem' : '1.5rem'} />
+                    <Text ml={isMobileVersion ? undefined : '4'} mt={isMobileVersion ? '4' : undefined} fontSize={isMobileVersion ? '1rem' : '1.5rem'} fontWeight='regular' textAlign='left' lineHeight='120%'>
                         No caso de dúvidas, nos mande uma DM!
                     </Text>
                 </Flex>
